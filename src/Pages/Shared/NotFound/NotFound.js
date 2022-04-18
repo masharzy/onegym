a9b4be9083411ef1ca4724f16177d3d0d24ba820
masharzy <horsnbd@gmail.com>
2022-04-18 23:01:36 +0600

    class replace to className

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,23 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './NotFound.css'
+import './NotFound.css';
 
 const NotFound = () => {
   return (
     <div className="container">
-      <div class="row justify-content-center align-items-center fourZeroFour-row">
-        <div class="col-md-12 col-sm-12">
+      <div className="row justify-content-center align-items-center fourZeroFour-row">
+        <div className="col-md-12 col-sm-12">
           <div
-            class="card border-0 mx-auto"
+            className="card border-0 mx-auto"
           >
-            <h3 class="display-1 text-muted text-center">404</h3>
+            <h3 className="display-1 text-muted text-center">404</h3>
 
-            <span class="card-subtitle mb-2 text-muted text-center">
+            <span className="card-subtitle mb-2 text-muted text-center">
               Page Could Not Be Found
             </span>
 
-            <div class="card-body mx-auto">
-              <Link to="/" class="btn btn-info text-white">
+            <div className="card-body mx-auto">
+              <Link to="/" className="btn btn-info text-white">
                 Back To Home
               </Link>
             </div>
